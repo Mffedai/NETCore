44d36b2bcfd5a384d9758082686ce17ee5e20093
Mffedai <mfatihfedai@gmail.com>
2024-03-09 15:32:41 +0300

    first project

---
+++ NET101/ConsoleProgramming/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Adınızı giriniz : ");
+String name = Console.ReadLine();
+Console.WriteLine("Soyadınızı giriniz : ");
+String surname = Console.ReadLine();
+Console.WriteLine("Hoşgeldiniz " + name + " " + surname);
